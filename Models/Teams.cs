616ce0f4b1ce5fa20fcf96b60fd17a6c57938d97
Gsaganichaitanya <chaitanyagasagani@gmail.com>
2023-12-29 15:26:42 +0530

    webAPI CodeFirst

---
+++ Models/Teams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Assignment15WebAPICodefirst.Models
+{
+    [Table("Teams")]
+    public class Teams
+    {
+        [Key]
+        public int TId { get; set; }
+        public string TName { get; set; }
+        public string TEmail { get; set; }
+        public string TSlogan { get; set; }
+    }
+}
